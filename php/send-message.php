7200d6785f6bdc898da5f38e227b9fb91c8612ad
Roberto Ruiz Flores <roruizf@gmail.com>
2020-11-21 23:52:52 -0300

    First webpage version ready to be uploaded and checked online

---
+++ php/send-message.php
@@ -0,0 +1,23 @@
+
+<?php
+$name_lastname = $_POST['name'] ."-".$_POST['lastname'];
+$mail = $_POST['email'];
+// $message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "This message was sent by  " . $name_lastname . ",\r\n";
+$message .= "Your e-mail is: " . $mail . " \r\n";
+$message .= "Subject: " . $_POST['message'] . " \r\n";
+$message .= "Sent on: " . date('d/m/Y', time());
+
+$to = 'roruizf@gmail.com';
+$subject = $_POST['subject'];
+
+mail($to, $subject, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
